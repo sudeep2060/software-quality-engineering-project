f1e5d158735db12076649a71ce69197332f725ef
Sudeep <bhattaraisudeep188@gmail.com>
2026-07-19 13:54:24 +0545

    Fix GitHub Actions Chrome setup

--- selenium_automation/utils/driver_factory.py
+++ selenium_automation/utils/driver_factory.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 def get_driver():
+    options = Options()
+
+    # Required for GitHub Actions
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
     driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install())
+        service=Service(ChromeDriverManager().install()),
+        options=options
     )
 
-    driver.maximize_window()
     driver.implicitly_wait(10)
 
     return driver
